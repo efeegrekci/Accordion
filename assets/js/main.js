4af95a8b82b1f3caa51770fc37275ab95494fd67
efeegrekci <efe.egrekci@gmail.com>
2022-03-11 05:46:55 +0300

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,45 @@
 $(document).ready(function () {
+
+    // Resize Page Refresh
+    $(window).bind('resize', function (e) {
+        if (window.RT) clearTimeout(window.RT);
+        window.RT = setTimeout(function () {
+            this.location.reload(false);
+        }, 100);
+    });
+    // Resize Page Refresh
+
     // Accordion Load
-    setTimeout(function () {
-        $('.accordion .accordionColumn').each(function () {
+    $(window).on("load", function () {
+        $('.accordion .accordionItem').each(function () {
             if ($(this).hasClass('open')) {
-                var accordionHeight = $(this).find('.desc').innerHeight();
-                var accordionTitle = $(this).find('.title').innerHeight();
-                var accordionHeightTotal = accordionHeight + accordionTitle;
-                $(this).css('height', accordionHeightTotal);
+                var descHeight = $(this).find('.desc').innerHeight();
+                var titleHeight = $(this).find('.title').innerHeight();
+                var totalHeight = descHeight + titleHeight;
+                $(this).css('height', totalHeight);
             } else {
                 $(this).css('height', $(this).find('.title').innerHeight(), 'px');
             }
         });
-    }, 100);
+    })
     // Accordion Load
 
     // Accordion Click
-    $('.accordion .title').click(function (e) {
-        var accordionHeight = $(this).siblings('.desc').innerHeight();
-        var accordionTitle = $(this).innerHeight();
-        var accordionHeightTotal = accordionHeight + accordionTitle;
-        if ($(this).parent('.accordionColumn').hasClass('open')) {
-            $(this).parent('.accordion .accordionColumn').css('height', accordionTitle, 'px');
-            $(this).parent('.accordionColumn').removeClass('open');
+    $('.accordion .title').click(function () {
+        var descHeight = $(this).siblings('.desc').innerHeight();
+        var titleHeight = $(this).innerHeight();
+        var totalHeight = descHeight + titleHeight;
+        if ($(this).parent('.accordionItem').hasClass('open')) {
+            $(this).parent('.accordion .accordionItem').css('height', titleHeight, 'px');
+            $(this).parent('.accordionItem').removeClass('open');
         } else {
-            $('.accordion .accordionColumn').removeClass('open');
-            $('.accordion .accordionColumn').each(function () {
+            $('.accordion .accordionItem').removeClass('open');
+            $('.accordion .accordionItem').each(function () {
                 $(this).css('height', $(this).find('.title').innerHeight() + 'px');
             });
-            $(this).parent('.accordionColumn').addClass('open');
-            $(this).parents('.accordion .accordionColumn').css('height', accordionHeightTotal);
+            $(this).parent('.accordionItem').addClass('open');
+            $(this).parents('.accordion .accordionItem').css('height', totalHeight);
         }
     });
-     // Accordion Click
+    // Accordion Click
 });
